eabb6863aeeb976bbc200bac7fdd3e1f220f4b0c
Hannah-KH <hannah@kingstonhendry.co.uk>
2023-01-14 10:53:47 +0000

    Add more logic to validation, record user inputs and log to console

--- assets/script.js
+++ assets/script.js
@@ -91,19 +91,73 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  // variable stores password length based on user input
+  let confirmLength = parseInt(
+    prompt("How long do you want your password to be?")
+    )
+ // This is not working at the moment
+   if(isNaN(confirmLength === true)){
+   alert("Password length must only include digits 0 to 9.");
+    return;
+   }
+    // At the moment, user can enter a number < 10, alerts pops up, then they can enter a number > 64, no alert pops up and they continue with the other prompts (same happens if entering bigger number first)
+    if (confirmLength < 10){
+      alert("Password must be 10 characters or more.");
+      prompt("How long do you want your password to be?");
+    } else if (confirmLength > 64){
+      alert("Password must be 64 characters or fewer.");
+      prompt("How long do you want your password to be?");
+    } else if (confirmLength < 10){
+      alert("Password must be 10 characters or more.");
+      prompt("How long do you want your password to be?");
+    }
+  
+    let confirmNumber = 
+    confirm("Would you like to include numbers?");
+    
+    let confirmLower = 
+    confirm("Would you like to include lower case letters?");
+    
+    let confirmUpper = 
+    confirm("Would you like to include upper case letters?");
+    
+    let confirmSpecial = 
+    confirm("Would you like to include special characters?");
+
+    if(confirmNumber === false &&
+      confirmLower === false &&
+      confirmUpper === false &&
+      confirmSpecial === false){
+        alert("You must select at least one type of character.\n\nSelect Generate password to start again.");
+        return;
+      }
+
+      let passwordOptions = {
+        confirmLength: confirmLength,
+        confirmNumber: confirmNumber,
+        confirmLower: confirmLower,
+        confirmUpper: confirmUpper,
+        confirmSpecial: confirmSpecial
+      }
+
+      console.log(passwordOptions);
 
 }
+
 
 // Function for getting a random element from an array
 // Should return a value
 function getRandom(arr) {
-return arr[Math.floor(Math.random()*arr.length)];
+let randomIndex = Math.floor(Math.random() * arr.length);
+let randomElement = arr[randomIndex];
+return randomElement; 
 }
+//console.log(random, numericCharacters[random]);
 
 // Function to generate password with user input
 // Should return a value
 function generatePassword() {
-
+let options = getPasswordOptions();
 }
 
 // Get references to the #generate element
@@ -119,25 +173,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-// User is prompted to choose how long to make the password, and which kind of elements to include
-
-var confirmLength = prompt("How long do you want your password to be?");
-
-if (confirmLength < 10){
-  alert("Password must be 10 characters or more.");
-  var confirmLength = prompt("How long do you want your password to be?");
-} else if (confirmLength > 64){
-  alert("Password must be 64 characters or fewer.");
-  var confirmLength = prompt("How long do you want your password to be?");
-};
-
-var confirmNumber = confirm("Would you like to include numbers?");
-
-var confirmLower = confirm("Would you like to include lower case letters?");
-
-var confirmUpper = confirm("Would you like to include upper case letters?");
-
-var confirmSpecial = confirm("Would you like to include special characters?");
-
-
